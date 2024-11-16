65eef363ade99d17cc0d48a8d1c44d5bfae21914
Karl Tay <karltay52@gmail.com>
2024-11-16 14:16:31 +0000

    step 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -11,6 +14,11 @@ function App() {
     <>
     <div>
     <WelcomeMessage />
+    </div>
+    <div>
+    <Header/>
+    <MainContent/>
+    <Footer/>
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">
