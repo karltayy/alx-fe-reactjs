5ff338e6db534cefa0b718229e7c4b352d500e7e
Karl Tay <karltay52@gmail.com>
2024-11-18 19:04:28 +0000

    main content and footer styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
+        <main style={{ padding: '20px', backgroundColor: '#f0f8ff', color: '#333', textAlign: 'center' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
     );
 }
 
