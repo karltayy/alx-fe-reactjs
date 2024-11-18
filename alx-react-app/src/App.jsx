55b09e06cb5358a600e3a0f7344bc5fe5abbaf8e
Karl Tay <karltay52@gmail.com>
2024-11-18 18:47:25 +0000

    new addition

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -18,6 +19,9 @@ function App() {
     <Header />
     <MainContent />
     <Footer />
+    <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
