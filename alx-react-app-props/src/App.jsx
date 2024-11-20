e4969ff7d6d5bb19a8c764f46b60d8d34cd55b95
Karl Tay <karltay52@gmail.com>
2024-11-20 10:02:10 +0000

    userData

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider>
-<ProfilePage userData={userData} />;
+    <UserContext.Provider value={userData}>
+<ProfilePage />;
     </UserContext.Provider>
 
   ) 
