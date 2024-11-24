c6014f5d2e01e935e9959c04a2f987ca60d7d0fa
Karl Tay <karltay52@gmail.com>
2024-11-24 21:22:34 +0000

    update

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
+// Fetch posts from the JSONPlaceholder API
 const fetchPosts = async () => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
   return response.data;
 };
 
 const PostsComponent = () => {
-  const { data, isLoading, isError, error, refetch } = useQuery('posts', fetchPosts);
+  const { data, isLoading, isError, error, refetch, isFetching } = useQuery(
+    'posts', // Query key
+    fetchPosts,
+    {
+      cacheTime: 5 * 60 * 1000, // Cache the data for 5 minutes (default: 5 minutes)
+      staleTime: 30 * 1000, // Mark data as fresh for 30 seconds (default: 0)
+      refetchOnWindowFocus: true, // Refetch data when the window regains focus (default: true)
+      keepPreviousData: true, // Keep previous data while fetching new data (default: false)
+    }
+  );
 
   if (isLoading) {
     return <p>Loading posts...</p>;
@@ -20,7 +30,9 @@ const PostsComponent = () => {
 
   return (
     <div>
-      <button onClick={refetch}>Refetch Posts</button>
+      <button onClick={refetch}>
+        {isFetching ? 'Refetching...' : 'Refetch Posts'}
+      </button>
       <ul>
         {data.map((post) => (
           <li key={post.id}>
