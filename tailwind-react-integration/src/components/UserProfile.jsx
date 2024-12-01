86543e1a0cb2551884b0a9f7918f707e66be0293
Karl Tay <karltay52@gmail.com>
2024-12-01 10:39:30 +0000

    effects

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg text-center hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
+        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-ou">John Doe</h1>
         <p className="text-sm sm:text-base text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
