7f4064bc178792bebc691048fb548118a25e3ab1
Karl Tay <karltay52@gmail.com>
2024-12-01 10:02:20 +0000

    tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
