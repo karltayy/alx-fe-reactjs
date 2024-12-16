09171977a685c0417b49733728a2eb3b5a456705
Karl Tay <karltay52@gmail.com>
2024-12-16 19:37:40 +0000

    recipe sharing platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // Or 'media' / 'class' based on preference
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
